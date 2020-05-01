25312fab59704a30fa4232e62f036e538ab0c8e1
Regis Yamamoto <regisyamaoto@gmail.com>
2020-05-01 18:38:44 -0300

    Primeiro commit

---
+++ how to use scroll.js
@@ -0,0 +1,9 @@
+INSERIR <script scr="js/script.js"></script> ABAIXO DE TODOS OS SCRIPTS DA PÁGINA index.html.
+
+COLOCAR ARQUIVO script.js NA PASTA JS.
+
+INSERIR id="nomedoid" NO <article></article>, FICANDO <article id="nomedoid"></article>.
+
+INSERIR #nomedoid NO LINK, FICANDO <a href="#nomedoid"></a>
+
+INSERIR id="top-menu" EM <ul class="nav navbar-nav"></ul>, FICANDO <ul class="nav navbar-nav" id="top-menu"></ul>
